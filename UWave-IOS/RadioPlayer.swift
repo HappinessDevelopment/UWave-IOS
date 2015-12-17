82bbfe6f9fc1fc873c900fc588f7d39d141cb47b
george <asterhousemusic@gmail.com>
2015-12-16 19:31:25 -0800

    Initial commit

---
+++ UWave-IOS/RadioPlayer.swift
@@ -0,0 +1,40 @@
+//
+//  RadioPlayer.swift
+//  RadioApp
+//
+//  Created by Jonathan Velazquez on 11/7/15.
+//  Copyright © 2015 Jonathan Velazquez. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class RadioPlayer {
+    static let sharedInstance = RadioPlayer()
+    private var player = AVPlayer(URL: NSURL(string: "http://live.uwave.fm:8000/listen-128.mp3")!)
+    
+    private var isPlaying = false
+    
+    func play() {
+        player.play()
+        isPlaying = true
+    }
+    
+    func pause() {
+        player.pause()
+        isPlaying = false
+    }
+    
+    func toggle() {
+        if isPlaying == true {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func currentlyPlaying() -> Bool {
+        return isPlaying
+    }
+    
+}
---
+++ UWave-IOS/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  RadioApp
+//
+//  Created by Jonathan Velazquez on 11/7/15.
+//  Copyright © 2015 Jonathan Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var playButton: UIButton!
+
+
+ 
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view, typically from a nib.
+        //playButton.setTitle("Play", forState: UIControlState.Normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        toggle()
+    }
+    func toggle(){
+        if RadioPlayer.sharedInstance.currentlyPlaying(){
+            pauseRadio()
+        }else{
+            playRadio()
+        }
+    }
+    func playRadio(){
+        
+        let imageName = "UWavePauseButton.png"
+        
+        let image2 = UIImage(named: imageName)
+
+        
+        RadioPlayer.sharedInstance.play()
+            //playButton.setTitle("Pause", forState: UIControlState.Normal)
+        
+            playButton.setImage(image2, forState: UIControlState.Normal)
+        
+    }
+    func pauseRadio(){
+        let imageName3 = "UWavePlayButton.png"
+        let image3 = UIImage(named: imageName3)
+        
+        RadioPlayer.sharedInstance.pause()
+        //playButton.setTitle("Play", forState: UIControlState.Normal)
+        playButton.setImage(image3, forState: UIControlState.Normal)
+        
+    }
+    
+        
+    }
+
+
+
+
